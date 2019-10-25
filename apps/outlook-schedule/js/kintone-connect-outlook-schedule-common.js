6e1784fe8c6a9627100c68f864f168839855a7cc
miyass <exe.miyakura0629@gmail.com>
2019-10-25 09:56:36 +0900

    fix outlook-schedule folder

--- apps/outlook-schedule/js/kintone-connect-outlook-schedule-common.js
+++ apps/outlook-schedule/js/kintone-connect-outlook-schedule-common.js
@@ -1,34 +1,44 @@
 window.kintoneAzureConnect = {
 
-    azure: {
-        clientId: 'cd8eaf08-e4eb-4ffd-99bb-8533132cb036',
-        graphApiScorp: ['https://graph.microsoft.com/calendars.readwrite'],
-        access: ['calendars.readwrite'],
-        eventUrl: 'https://graph.microsoft.com/v1.0/me/events'
+  config: {
+    auth: {
+      clientId: 'b689274d-3ed5-429d-aa7a-23a2d446af0e',
+      authority: 'https://login.microsoftonline.com/common'
     },
-
-
-    kintone: {
-
-        fieldCode: {
-
-            // Field code of subject
-            subject: 'To_Do',
-
-            // Field code of body
-            body: 'Details',
-
-            // Field code of start
-            startDate: 'From',
-
-            // Field code of end
-            endDate: 'To',
-
-            // Field code of eventId
-            eventId: 'EventId',
-
-            // Field code of attachFile
-            attachFile: 'Attachments'
-        }
+    cache: {
+      cacheLocation: "localStorage",
+      storeAuthStateInCookie: true
     }
+  },
+
+  graphApiScorp: {
+    scopes: ['calendars.readwrite'],
+  },
+
+  eventUrl: 'https://graph.microsoft.com/v1.0/me/events',
+
+  kintone: {
+    fieldCode: {
+
+      // Field code of subject
+      subject: 'To_Do',
+
+      // Field code of body
+      body: 'Details',
+
+      // Field code of start
+      startDate: 'From',
+
+      // Field code of end
+      endDate: 'To',
+
+      // Field code of eventId
+      eventId: 'EventId',
+      
+      // Field code of attachFile
+      attachFile: 'Attachments'
+    }
+  }
 };
+
+
