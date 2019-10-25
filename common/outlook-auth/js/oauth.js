99085fa3c56b2a6ed7f0d57068b362def23d6ddb
miyass <exe.miyakura0629@gmail.com>
2019-10-25 09:54:23 +0900

    fix common folder

--- common/outlook-auth/js/oauth.js
+++ common/outlook-auth/js/oauth.js
@@ -1,38 +1,37 @@
 /* global Msal */
 (function() {
-    'use strict';
+  'use strict';
 
-    var KAC = window.kintoneAzureConnect;
+  var KAC = window.kintoneAzureConnect;
 
-    var azureOauth = {
+  var azureOauth = {
+    
+    userAgentApplication: null,
 
-        userAgentApplication: null,
+    getUserInfo: function() {
+      return this.userAgentApplication.getAccount().userName;
+    },
 
-        getUserInfo: function() {
-            return this.userAgentApplication.getUser();
-        },
+    init: function() {
+      var self = this;
+      self.userAgentApplication = new Msal.UserAgentApplication(KAC.config);
+    },
 
-        init: function() {
-            this.userAgentApplication = new Msal.UserAgentApplication(KAC.azure.clientId, null, function(
-                errorDes, token, error, tokenType) {});
-            return this.userAgentApplication.getUser();
-        },
+    signIn: function() {
+      var self = this;
+      return self.userAgentApplication.loginPopup(KAC.graphApiScorp);
+    },
 
-        signIn: function() {
-            var self = this;
-            return self.userAgentApplication.loginPopup(KAC.azure.access);
-        },
+    signOut: function() {
+      var self = this;
+      self.userAgentApplication.logout();
+    },
 
-        signOut: function() {
-            var self = this;
-            self.userAgentApplication.logout();
-        },
+    callGraphApi: function() {
+      var self = this;
+      return self.userAgentApplication.acquireTokenSilent(KAC.graphApiScorp);
+    }
+  };
 
-        callGraphApi: function() {
-            var self = this;
-            return self.userAgentApplication.acquireTokenSilent(KAC.azure.graphApiScorp);
-        }
-    };
-
-    window.azureOauth = azureOauth || {};
+  window.azureOauth = azureOauth || {};
 }());
